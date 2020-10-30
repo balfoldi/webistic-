a74c6432aa7bc791b22eb5d4a3cb5a64c5c842c3
BorisSIUS <balfooldi@yahoo.fr>
2020-10-30 01:59:55 +0100

    done

--- src/components/CheckLanguageDisplay/index.jsx
+++ src/components/CheckLanguageDisplay/index.jsx
@@ -7,11 +7,12 @@ import LanguageContext from "./../../LanguageContext";
 const CheckLanguageDisplay = () => {
   const language = useContext(LanguageContext);
 
+  const languageButtun= {}
   
   return (
     <>
-      <Image onClick={() => {language.swichLanguage("en"); console.log("noob")}} src={en} width="35px" height="39px" alt="english" roundedCircle />
-      <Image onClick={() => {language.swichLanguage("fr"); console.log("noob")}} src={fr} width="35px" height="39px" alt="french" roundedCircle />
+      {language.local === "en" || <Image onClick={() => {language.swichLanguage("en")}} src={en} width="35px" height="39px" alt="english" roundedCircle />}
+      {language.local === "fr" || <Image onClick={() => {language.swichLanguage("fr")}} src={fr} width="35px" height="39px" alt="french" roundedCircle />}
     </>
   );
 };
